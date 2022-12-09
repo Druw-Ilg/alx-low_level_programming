85d377d9eaa44082b30fa323f11091f8544e605f
Druw-Ilg <ilagouandrew@gmail.com>
2022-12-09 09:25:22 -0800

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+*main - the function
+*Return: the result
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a' ; letter <= 'z' ; letter++)
+{
+putchar(letter);
+}
+return (0);
+}
