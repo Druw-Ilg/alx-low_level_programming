8a4b7dfbb68f71ba0decf408135388f8f4736c82
Druw-Ilg <ilagouandrew@gmail.com>
2022-12-09 21:26:23 -0800

    Patience...

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,15 @@
 int main(void)
 {
 int num;
+int comma = ',';
+int space = ' ';
+int separator = comma + space;
 for (num = '0' ; num <= '9' ; num++)
 {
 if (!(num == '9'))
 {
 putchar(num);
-putchar(',' + ' ');
+putchar(separator);
 }
 else
 {
