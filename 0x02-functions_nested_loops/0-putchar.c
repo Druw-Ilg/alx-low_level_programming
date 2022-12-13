1fa97f1a33ef32e6b873797ea086a5d463f7c016
Druw-Ilg <ilagouandrew@gmail.com>
2022-12-13 14:00:56 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,16 @@
 /**
 * main - give alaphabet
 * Return: output the result
+*
+*@c: The character to print
+*
 */
+int _putchar(char c);
 int main(void)
 {
 char ch[] = "_putchar\n";
 int i, len = sizeof(ch) / sizeof(ch[0]);
 for (i = 0 ; i < len ; i++)
-putchar(ch[i]);
+_putchar(ch[i]);
 return (0);
 }
