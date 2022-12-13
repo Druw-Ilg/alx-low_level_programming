69dc040567e7ffcd6666a95ffbfef3e65940b0ea
Druw-Ilg <ilagouandrew@gmail.com>
2022-12-12 22:34:19 -0800

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - give alaphabet
+* Return: output the result
+*/
+int main(void)
+{
+char quote[] = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+int len = sizeof(quote) / sizeof(quote[0]);
+int i;
+for (i = 0 ; i < len ; i++)
+putchar(quote[i]);
+putchar('\n');
+return (0);
+}
